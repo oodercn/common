591f95bd01ea3bce3b963b26ee7bfafca584bdd6
18618287247 <18683731@qq.com>
2025-11-30 17:15:45 +0800

    修复，http参数默认数组问题

--- ooder-common-client/src/main/java/net/ooder/common/util/ClassUtility.java
+++ ooder-common-client/src/main/java/net/ooder/common/util/ClassUtility.java
@@ -150,6 +150,19 @@
  * <p>
  * License: MIT License
  * </p>
+ * <p>
+ * $RCSfile: ClassUtility.java,v $
+ * $Revision: 1.0 $
+ * $Date: 2025/08/25 $
+ * <p>
+ * Copyright (c) 2025 ooder.net
+ * </p>
+ * <p>
+ * Company: ooder.net
+ * </p>
+ * <p>
+ * License: MIT License
+ * </p>
  */
 /**
  * $RCSfile: ClassUtility.java,v $
